b27f8c1184ae26ee4c3ad753428196577fa31d83
Yash <pratapyash.yps@gmail.com>
2025-08-02 13:35:59 +0530

    Delete components directory

--- components/LoadingSpinner.tsx
+++
@@ -1,7 +0,0 @@
-export function LoadingSpinner() {
-  return (
-    <div className="flex justify-center items-center py-12">
-      <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-farcaster-purple"></div>
-    </div>
-  )
-}
