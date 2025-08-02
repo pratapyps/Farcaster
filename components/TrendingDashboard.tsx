dc7a24a0a0374e02e15440ef69f43b870492e0cb
Yash <pratapyash.yps@gmail.com>
2025-08-02 13:36:16 +0530

    Add files via upload

---
+++ components/TrendingDashboard.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+import { TrendingTopics } from './TrendingTopics'
+
+export function TrendingDashboard() {
+  return (
+    <div className="max-w-6xl mx-auto">
+      <TrendingTopics />
+    </div>
+  )
+}
