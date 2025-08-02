c46fdb0e1e77a88c1222e563bfeac33c5ef1de6d
Yash <pratapyash.yps@gmail.com>
2025-08-02 13:50:51 +0530

    Add files via upload

--- components/TrendingDashboard.tsx
+++ components/TrendingDashboard.tsx
@@ -1,11 +1,18 @@
 'use client'
 
-import { TrendingTopics } from './TrendingTopics'
+import { useEffect } from 'react';
+import { sdk } from '@farcaster/miniapp-sdk';
+import { TrendingTopics } from './TrendingTopics';
 
 export function TrendingDashboard() {
+  useEffect(() => {
+    // Notifies Farcaster that the mini app is ready and can hide the splash screen
+    sdk.actions.ready();
+  }, []);
+
   return (
     <div className="max-w-6xl mx-auto">
       <TrendingTopics />
     </div>
-  )
+  );
 }
